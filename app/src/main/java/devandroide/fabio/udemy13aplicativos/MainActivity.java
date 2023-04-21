64ad14f03146b4ade63e4310e10569a22d68f7cb
projeto13aplicatiovs <fabiocorrena@gmail.com>
2023-04-21 17:13:10 -0300

    primeiros aplicaivos

---
+++ app/src/main/java/devandroide/fabio/udemy13aplicativos/MainActivity.java
@@ -0,0 +1,14 @@
+package devandroide.fabio.udemy13aplicativos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
